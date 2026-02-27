faaba774a579c1384f9c35987c0d8a368ca00872
Ngidi Enterprise <ngidiproject@gmail.com>
2026-02-27 19:57:05 +0200

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -1304,6 +1304,7 @@ function App() {
                       onShowUpload={() => setShowLocalProductUpload(true)}
                       initialProducts={localProductsCache}
                       onAddToCart={addToCart}
+                      onSearch={setSearchTerm}
                     />
                   ) : (
                     <LocalProductDetail
